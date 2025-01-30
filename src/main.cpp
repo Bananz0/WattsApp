c1d202ca26e25f55a02012fd80c717fb9f0b7035
Glen M <glenmuthokaq@gmail.com>
2025-01-30 17:29:01 +0000

    cmake wrong ammended gitgnore

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,12 @@
-#include <iostream>
+#include <stdio.h>
+#include <avr/io.h>
 
 
 int main() {
+    DDRB = 0xFF;
+    PORTB = 0x00;
+    while (1) {
+        PORTB ^= (1 << PB5);
+    }
     return 0;
 }
