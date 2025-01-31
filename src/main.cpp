08d532c8bc457a6de96e9a94afd61a2413ab050c
Glen M <glenmuthokaq@gmail.com>
2025-01-31 15:22:46 +0000

    Completed Pin defintion

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,19 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
-
+void signOfLife(void) {
+    PORTB ^= (1 << PB7);
+    _delay_ms(500);
+}
 
 //Function to test light for a specific time delay (seconds)
 void testLight(const uint8_t time_delay) {
-    uint8_t i = time_delay/50;
     DDRB |= (1 << PB7); // Set PB7 as output
     PORTB &= ~(1 << PB7); // Start with PB7 low
 
-    while (i<20) {
+    for (uint8_t i = 0; i < (time_delay * 1000) / 50; i++){
         PORTB ^= (1 << PB7); // Toggle PB7
         _delay_ms(50);       // Delay 50ms
-        i+=1;
     }
 }
 
@@ -33,13 +34,48 @@ void initializePorts(const char portName, const PortType portType ) { //This is 
     }
 }
 
-int main() {
+void finalizePorts() {
     /*portType Input = 0x00 , Output = 0xFF*/
-    initializePorts('A', OUTPUT); // Set Port A to Analog Output (from SM to TB ( Mains Capacity, Analog Ground Ref for Mains Capacity))
-    initializePorts('B', INPUT); // Set Port B to Analog Input (from TB to SM ( Power Generation Stats ) )
-    initializePorts('C', OUTPUT); // Set Port C to Digital Output ( from SM to TB ( Charge batt, discharge batt, etc...))
+    initializePorts('A', INPUT); // Set Port A to Analog Output (from SM to TB ( Mains Capacity, Analog Ground Ref for Mains Capacity))
+    initializePorts('B', OUTPUT); // Set Port B to Analog Input (from TB to SM ( Power Generation Stats ) )
+    initializePorts('C', OUTPUT); // Set Port C to Digital Output ( from SM to TB ( Charge batt, discharge batt, eetc...))
     initializePorts('D', INPUT); // Set Port D to Digital Input (from TB to SM ( Call for Load 1,2,3, ground ref)
 
+    //Port A pin defs
+    //Will need to use ADC
+    //Redundant but set 4 pins as explicit inputs
+    DDRA &= ~(_BV(0) /*Busbar Voltage*/
+           | _BV(1) /*Busbar Current*/
+           | _BV(2) /*Wind Turbine Capacity*/
+           | _BV(3) /*PV Capacity*/);
 
-    return 0;
+    //Port B pin defs
+    //Analog Output
+    DDRB |= _BV(0); //Mains Capacity (0-10v)
+
+
+    //Port C pin defs
+    //Digital Outputs
+    DDRC |= (_BV(0) /*Charge Battery*/
+         | _BV(1) /*Discharge Battery*/
+         | _BV(2) /*Load 1 Switch*/
+         | _BV(3) /*Load 2 Switch*/
+         | _BV(4) /*Load 3 Switch*/); //this took way tooo long bruh
+
+    //Port D pin defs
+    //Digital Inputs
+    PORTD = 0xFF;
+    DDRD &= (~_BV(0) /*Call for Load 1*/
+           | ~_BV(1) /*Call for Load 2*/
+           | ~_BV(2) /*Call for Load 3*/);
+}
+
+int main() {
+    finalizePorts();
+    testLight(6);
+    // ReSharper disable once CppDFAEndlessLoop
+    while (true) {
+        signOfLife(); //Blink LED every .5 sec to show sign of life
+
+    }
 }
