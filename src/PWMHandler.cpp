e835216c2a0ec7d83b5c0edfba390dd15a8947e4
Glen M <glenmuthokaq@gmail.com>
2025-02-04 18:42:43 +0000

    populated initializeTimer1 with Jackson's code

--- src/PWMHandler.cpp
+++ src/PWMHandler.cpp
@@ -2,15 +2,18 @@
 // Created by glenm on 2/2/2025.
 //
 
+#define TARGET_TIME_MS 100
+#define PRESCALER 64
 #include "PWMHandler.h"
 
 PWMHandler::PWMHandler() {}
 
 void PWMHandler::initializePWM() { //Initialize the PWM based on pg155 of the ilMatto datasheet
     //Using PD5
-    //initializeTimer1();
+    initializeTimer1();
     //Using PD7
     initializeTimer2();
+
 }
 
 void PWMHandler::setOutputVoltage(float voltage) { //PD6 and PD7 for Amplitude Modulation
@@ -21,15 +24,10 @@ void PWMHandler::setOutputVoltage(float voltage) { //PD6 and PD7 for Amplitude M
 }
 
 void PWMHandler::initializeTimer1() {
-    TCCR2A |=  _BV(COM2A1)              /* Clear OC2A on Compare Match*/
-   |   _BV(WGM21)                       /* Fast PWM mode */
-   |   _BV(WGM20);	                    /* 0xFF, BOTTOM */
-    TCCR2A &= ~_BV(COM2A0);             /* Set OC2A at bottom*/
-    TCCR2B = _BV(CS20);		        	// No Prescaling
-    ASSR = 0;			            	// I/O clock
-    TIMSK2 = 0;			            	// No interrupts
-    TCNT2 = 0x00;	        			// Counter 1
-    OCR2A = 0xFF;			         	// Compare Value
+    TCCR1B |= (1 << WGM12);
+    OCR1A = (F_CPU / (PRESCALER * 1000)) * TARGET_TIME_MS - 1;
+    TIMSK1 |= (1 << OCIE1A);
+    TCCR1B |= (1 << CS11) | (1 << CS10);
 }
 void PWMHandler::initializeTimer2() {
     TCCR2A |=  _BV(COM2A1)              /* Clear OC2A on Compare Match*/
