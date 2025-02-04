e052369aef8f019f88ec37a3c3a416b38347896c
Glen M <glenmuthokaq@gmail.com>
2025-02-04 18:42:16 +0000

    Wrapped more Pictor functions

--- src/DisplayHandler.h
+++ src/DisplayHandler.h
@@ -4,13 +4,24 @@
 
 #ifndef DISPLAYHANDLER_H
 #define DISPLAYHANDLER_H
+#include <stdint.h>
 
 
 class DisplayHandler {
-    public:
+public:
+    typedef enum {PORTRAIT = 0, LANDSCAPE = 1, PORTRAIT_INV = 2, LANDSCAPE_INV = 3} orientation;
+    typedef enum {ON = 1, OFF = 0} state;
+
     DisplayHandler();
     ~DisplayHandler();
-    void startDisplay(bool vsync);
+    static void startDisplay(bool vsync);
+    static void stopDisplay();
+    static void setBacklight(state State);
+    static void setOrientation(orientation rotation);
+    static void clearScreen();
+
+private:
+
 
 };
 
