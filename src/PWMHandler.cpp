a43ad78a3478b1b4fe40f30eae900710a0c02962
Glen M <glenmuthokaq@gmail.com>
2025-02-04 17:26:11 +0000

    Pictor

--- src/PWMHandler.cpp
+++ src/PWMHandler.cpp
@@ -7,18 +7,38 @@
 PWMHandler::PWMHandler() {}
 
 void PWMHandler::initializePWM() { //Initialize the PWM based on pg155 of the ilMatto datasheet
-    TCCR2A |=  _BV(COM2A1)    /* Clear OC2A on Compare Match*/
-           |   _BV(WGM21)     /* Fast PWM mode */
-           |   _BV(WGM20);	  /* 0xFF, BOTTOM */
-    TCCR2A &= ~_BV(COM2A0);   /* Set OC2A at bottom*/
+    //Using PD5
+    //initializeTimer1();
+    //Using PD7
+    initializeTimer2();
+}
+
+void PWMHandler::setOutputVoltage(float voltage) { //PD6 and PD7 for Amplitude Modulation
+    DDRD |= _BV(7);
+    const float scalingFactor = static_cast<float>(voltage) * 1000 / Vref;
+    //OCR2A = ((voltage * 1000)/ Vref ) * 0xFF;
+    OCR2A = (float) 0xFF * scalingFactor;
+}
+
+void PWMHandler::initializeTimer1() {
+    TCCR2A |=  _BV(COM2A1)              /* Clear OC2A on Compare Match*/
+   |   _BV(WGM21)                       /* Fast PWM mode */
+   |   _BV(WGM20);	                    /* 0xFF, BOTTOM */
+    TCCR2A &= ~_BV(COM2A0);             /* Set OC2A at bottom*/
     TCCR2B = _BV(CS20);		        	// No Prescaling
     ASSR = 0;			            	// I/O clock
     TIMSK2 = 0;			            	// No interrupts
     TCNT2 = 0x00;	        			// Counter 1
     OCR2A = 0xFF;			         	// Compare Value
 }
-
-void PWMHandler::setOutputVoltage(uint8_t voltage) { //PD6 and PD7 for Amplitude Modulation
-    DDRD |= _BV(7);
-    OCR2A = ((voltage * 1000)/ Vref ) * 0xFF;
+void PWMHandler::initializeTimer2() {
+    TCCR2A |=  _BV(COM2A1)              /* Clear OC2A on Compare Match*/
+       |   _BV(WGM21)                   /* Fast PWM mode */
+       |   _BV(WGM20);	                /* 0xFF, BOTTOM */
+    TCCR2A &= ~_BV(COM2A0);             /* Set OC2A at bottom*/
+    TCCR2B = _BV(CS20);		        	// No Prescaling
+    ASSR = 0;			            	// I/O clock
+    TIMSK2 = 0;			            	// No interrupts
+    TCNT2 = 0x00;	        			// Counter 1
+    OCR2A = 0xFF;			         	// Compare Value
 }
