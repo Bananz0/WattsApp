7c1b10d3411eefa569538b09a5483cbc139bf86b
Jackson.Liu <cl5y23@soton.ac.uk>
2025-02-06 13:58:56 +0000

    try to implement checkLoadCallChanges part and delete the earlier pin section

--- src/DigitalInput.cpp
+++ src/DigitalInput.cpp
@@ -3,6 +3,11 @@
 //
 
 #include "DigitalInput.h"
+
+bool lastLoad1Call = false;
+bool lastLoad2Call = false;
+bool lastLoad3Call = false;
+
 DigitalInput::DigitalInput() {
     //Explicitly set PIN C0, C1 and C2 as inputs.
     DDRC &= ~(_BV(0) | _BV(1) | _BV(2));
@@ -11,7 +16,12 @@ DigitalInput::DigitalInput() {
     //Enable Pin Change Registers
     PCICR |= _BV(PCIE2); //Pin change regiser for PortC
     //Enable specific pin change mask for the port c
-    PCMSK2 |= _BV(PCINT16)|_BV(PCINT17)| _BV(PCINT18); ;
+    PCMSK2 |= _BV(PCINT16)|_BV(PCINT17)| _BV(PCINT18);
+
+    //Set the load switches PB1/PB2/PB3 as output
+    DDRB |= (1 << PB3) | (1 << PB4) | (1 << PB5);
+    //Turn off the load switches
+    PORTB &= ~((1 << PB3) | (1 << PB4) | (1 << PB5));
 }
 bool DigitalInput::readLoad1Call() { //Pin C0 Call for Load 1 (1 - on 0 - off)
     return (PINC & (1 << PINC0));
@@ -23,5 +33,32 @@ bool DigitalInput::readLoad3Call() { //Pin C2 Call for Load 3 (1 - on 0 - off)
     return (PINC & (1 << PINC2));
 }
 void DigitalInput::checkLoadCallChanges() {
+    bool currentLoad1Call = readLoad1Call();
+    bool currentLoad2Call = readLoad2Call();
+    bool currentLoad3Call = readLoad3Call();
 
+    //load 1
+    if (currentLoad1Call && !lastLoad1Call) {
+        PORTB |= (1 << PB3);  //Load 1 on
+    } else if (!currentLoad1Call && lastLoad1Call) {
+        PORTB &= ~(1 << PB3); //OFF
+    }
+
+    //load 2
+    if (currentLoad2Call && !lastLoad2Call) {
+        PORTB |= (1 << PB4);  //ON
+    } else if (!currentLoad2Call && lastLoad2Call) {
+        PORTB &= ~(1 << PB4); //OFF
+    }
+
+    //load 3
+    if (currentLoad3Call && !lastLoad3Call) {
+        PORTB |= (1 << PB5);  //ON
+    } else if (!currentLoad3Call && lastLoad3Call) {
+        PORTB &= ~(1 << PB5); //OFF
+    }
+
+    lastLoad1Call = currentLoad1Call;
+    lastLoad2Call = currentLoad2Call;
+    lastLoad3Call = currentLoad3Call;
 }
