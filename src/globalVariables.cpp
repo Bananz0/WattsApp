176d9347c295847c875a50cf32e4f6cca02160ef
Glen M <glenmuthokaq@gmail.com>
2025-02-06 18:45:40 +0000

    a

--- src/globalVariables.cpp
+++ src/globalVariables.cpp
@@ -4,6 +4,7 @@
 
 #include "globalVariables.h"
 
-uint16_t Vref = 3300;                       //Reference Voltage, 3.3v
+volatile float Vref = 3.3;                       //Reference Voltage, 3.3v
 volatile uint16_t ADCVoltage;              //Global ADC voltage variable and idk if this is good
 volatile bool ADCConversionFlag = false;   //Global ADC Conversion Complete Flag
+volatile uint32_t rawADC = 0;
