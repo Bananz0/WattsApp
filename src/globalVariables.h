7d8700355cfbf419db95134829c0d429668c4f37
Glen M <glenmuthokaq@gmail.com>
2025-02-06 18:45:31 +0000

    a

--- src/globalVariables.h
+++ src/globalVariables.h
@@ -7,7 +7,7 @@
 #ifndef GLOBALVARIABLES_H
 #define GLOBALVARIABLES_H
 
-extern uint16_t Vref;                            //Reference Voltage, 3.3v
+extern volatile float Vref;                            //Reference Voltage, 3.3v
 extern volatile uint16_t ADCVoltage;             //Global - extern - ADC voltage variable and idk if this is good
 extern volatile bool ADCConversionFlag;
 
