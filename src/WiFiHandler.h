45b3d8159d28650fb5dc35b6d45a292b24417e2d
Glen M <glenmuthokaq@gmail.com>
2025-02-11 11:39:22 +0000

    barebones implementation of WifiHandler

--- src/WiFiHandler.h
+++ src/WiFiHandler.h
@@ -5,7 +5,28 @@
 #ifndef WIFIHANDLER_H
 #define WIFIHANDLER_H
 
+#include <stdint.h>
+#include "HardwareSerial.h"
+
 class WiFiHandler {
+public:
+    WiFiHandler(); //Initialize the esp8266 with the baud and everyghing else.
+    ~WiFiHandler();
+    bool connectToWiFi(const char* ssid, const char* password); //Connects to other universe
+    void disconnectFromWiFi();
+    bool isConnected();
+    bool sendToFireBase(const char* message);
+
+    void turnOnModule(); //Set the EN pin to 3.3v
+    void turnOffModule(); //Set the EN pin to 0.0025v
+    void resetModule(); //Have you tried turning it off and on again
+
+private:
+    uint8_t enablePin;
+    bool connected;
+    void sendATCommand(const char* command);
+    void waitForResponse();
+    HardwareSerial& espSerial;
 
 };
 
