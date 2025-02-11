53d50d4cf059d166c907fcfb734d6bbe5fc6b7dd
Glen M <glenmuthokaq@gmail.com>
2025-02-11 05:31:04 +0000

    wifi handler (not sure if we will have wifi working on this board) - pin limitation

---
+++ src/WiFiHandler.cpp
@@ -0,0 +1,5 @@
+//
+// Created by glenm on 2/10/2025.
+//
+
+#include "WiFiHandler.h"
---
+++ src/WiFiHandler.h
@@ -0,0 +1,14 @@
+//
+// Created by glenm on 2/10/2025.
+//
+
+#ifndef WIFIHANDLER_H
+#define WIFIHANDLER_H
+
+class WiFiHandler {
+
+};
+
+
+
+#endif //WIFIHANDLER_H
