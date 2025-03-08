d2e8b0a829eff9f0dfdf01862698af54a51d7055
Glen M <glenmuthokaq@gmail.com>
2025-03-08 10:57:30 +0000

    Changed the code structure slightly. We won't use the WiFiHandler.h as that's just recreating the wheel

---
+++ src/ESP8266Handler.cpp
@@ -0,0 +1,5 @@
+//
+// Created by glenm on 3/8/2025.
+//
+
+#include "ESP8266Handler.h"
---
+++ src/ESP8266Handler.h
@@ -0,0 +1,16 @@
+//
+// Created by glenm on 3/8/2025.
+//
+
+#ifndef ESP8266HANDLER_H
+#define ESP8266HANDLER_H
+
+
+
+class ESP8266Handler {
+
+};
+
+
+
+#endif //ESP8266HANDLER_H
